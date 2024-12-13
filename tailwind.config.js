d30512625c88c2cdf8c6952cd0b13e579d1864b1
moonye@live.fr <moonye@live.fr>
2024-12-13 14:57:57 +0100

    Fin de page

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,33 @@ module.exports = {
         'fond-quadrille': "url('/images//Fond quadrillé.jpg')",
         'gradient-clair-orange': 'linear-gradient(to bottom, var(--light-color) 68%, var(--secondary-color) 100%)',
       },
-      
+      fontFamily: {
+        changa: ['Changa One', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif'],
+      },
+      boxShadow: {
+        'inner-lg': 'inset 0 4px 8px rgba(0, 0, 0, 0.2)',  
+      },
+      // Ajouter le text-shadow dans les extensions de Tailwind
+      textShadow: {
+        'white': '2px 2px 4px rgba(255, 255, 255, 0.7)', // Ombre blanche autour du texte
+      },
     },
   },
 
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.btn-custom': {
+          '@apply mt-4 p-2 bg-primary text-black rounded-[30%] border-4 border-black w-[40%] font-changa text-lg': {},
+        },
+        '.btn-custom:hover': {
+          '@apply bg-dark text-light border-light': {},
+        },
+        '.btn-custom:focus': {
+          '@apply bg-dark text-light border-light ring-2 ring-light': {}, // Ajoute un ring lumineux lors du clic/focus
+        },
+      });
+    },
+  ],
 };
