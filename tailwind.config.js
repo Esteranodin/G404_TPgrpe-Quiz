a5323bdbb41087208172e1a4a632835610f860fb
moonye@live.fr <moonye@live.fr>
2024-12-13 12:40:04 +0100

    Debut design page 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./source/index.php"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  // Chemins où Tailwind doit rechercher les classes
+  content: ["./index.html", "./source/index.php"],
 
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)', // Marron
+        secondary: 'var(--secondary-color)', // Orange
+        light: 'var(--light-color)', // Clair
+        dark: 'var(--dark-color)', // Foncé
+        background: 'var(--background-color)', // Fond
+      },
+      backgroundImage: {
+        'fond-quadrille': "url('/images//Fond quadrillé.jpg')",
+        'gradient-clair-orange': 'linear-gradient(to bottom, var(--light-color) 68%, var(--secondary-color) 100%)',
+      },
+      
+    },
+  },
+
+  plugins: [],
+};
