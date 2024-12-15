1c9777adbac5db6b8298d43fd0d1e32421b9971c
moonyelit <fougerouse.elodie@gmail.com>
2024-12-15 23:49:09 +0100

    PAGE 2 sans animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,13 @@ module.exports = {
     extend: {
       colors: {
         primary: 'var(--primary-color)', // Marron
+        primaryopacity: 'rgba(110, 67, 60, 0.78)',  // Marron- opacité
         secondary: 'var(--secondary-color)', // Orange
         light: 'var(--light-color)', // Clair
         dark: 'var(--dark-color)', // Foncé
         background: 'var(--background-color)', // Fond
+        graycustom: '#858494',  // Gris 
+
       },
       backgroundImage: {
         'fond-quadrille': "url('../images//Fond quadrillé.jpg')",
@@ -25,7 +28,9 @@ module.exports = {
         rubik: ['Rubik', 'sans-serif'],
       },
       boxShadow: {
-        'inner-lg': 'inset 0 4px 8px rgba(0, 0, 0, 0.2)',  
+        'inner-lg': 'inset 0 4px 8px rgba(0, 0, 0, 0.2)',
+        'inner-box': 'inset 0 15px 10px rgba(0, 0, 0, 0.2)',
+
       },
       // Ajouter le text-shadow dans les extensions de Tailwind
       textShadow: {
@@ -35,20 +40,20 @@ module.exports = {
         float: 'float 3s ease-in-out infinite',
         float2: 'float 3s ease-in-out infinite 1.5s', // Pour décaler l'animation
         'bg-scroll': 'bg-scrolling-reverse 1.92s linear infinite',
-
       },
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0px) translateX(0px)' },
           '50%': { transform: 'translateY(-8px) translateX(4px)' },
-          'bg-scrolling-reverse': {
-          '100%': { backgroundPosition: '50px 50px' },
         },
+        'bg-scrolling-reverse': {
+          '100%': { backgroundPosition: '50px 50px' },
         },
       },
     },
   },
 
+  // Ajout des plugins personnalisés
   plugins: [
     function ({ addComponents }) {
       addComponents({
@@ -59,9 +64,30 @@ module.exports = {
           '@apply bg-dark text-light border-light': {},
         },
         '.btn-custom:focus': {
-          '@apply bg-dark text-light border-light ring-2 ring-light': {}, // Ajoute un ring lumineux lors du clic/focus
+          '@apply bg-dark text-light border-light ring-2 ring-light': {},
+        },
+        '.btn-custom2': {
+          '@apply m-5 p-5 bg-primary text-light rounded-lg border-2 border-light w-[60%] font-changa text-3xl font-light shadow-lg ': {},
+        },
+        '.btn-custom2:hover': {
+          '@apply bg-dark': {},
+        },
+        '.btn-custom2:focus': {
+          '@apply bg-dark': {},
         },
       });
     },
-  ],
+
+    function ({ addUtilities }) {
+      addUtilities({
+        '.text-stroke': {
+          '-webkit-text-stroke': '4.5px #6E433C', 
+          'color': '#FEFBE8', 
+          'font-size': '4rem',  
+          'line-height': '1', 
+        },
+      }, ['responsive', 'hover']);
+    }
+  ]
 };
+
