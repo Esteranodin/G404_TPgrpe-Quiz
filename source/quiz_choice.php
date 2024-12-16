2da62f9613c0a1fdaa8a0211d92d07ba1e675b52
moonye@live.fr <moonye@live.fr>
2024-12-16 09:44:02 +0100

    ADD : Combiner php et html

--- source/quiz_choice.php
+++ source/quiz_choice.php
@@ -23,50 +23,146 @@ try {
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Quiz-Choice</title>
+    <link rel="stylesheet" href="../assets/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    
 
-<p>Bienvenue <?= $_SESSION["pseudo"]  ?></p>
-<h2>Choix du quiz</h2>
-<section class="container">
-    <?php
-    foreach($typesQuiz as $typeQuiz){
-        
-    ?>
-    <article>
-        <h3><?=  $typeQuiz["name"] ?></h3>
-        <hr>
-        <div>
-            <div>
-                <p></p>
-                <p></p>
+
+<body class="fond-quadrille animate-bg-scroll overflow-x-hidden bg-[#6E433C] bg-opacity-50">
+    <!-- Ajout d'une div avec une couleur de fond et une opacité de 40% -->
+    <!-- <div class="absolute inset-0 bg-[#6E433C] bg-opacity-50 z-0 h-screen w-full"></div> -->
+
+    <!-- Section principale -->
+    <section
+        class="relative h-full max-w-full m-10 p-3 bg-gradient-clair-orange border-t-[7px] border-l-[7px] border-r-[15px] border-b-[15px] border-primary rounded-[42px]">
+        <!-- Image en haut de la section -->
+
+        <header>
+            <div class="absolute top-[-1rem] left-0 w-full flex justify-center items-start">
+                <img src="../images/Phone - Quiz question/Group 32.png" alt="Image décorative" class="w-[120%] max-w-none">
             </div>
-            <div>
-                <p></p>
-                <p></p>
-            </div>
-            <div>
-                <p></p>
-                <p></p>
-            </div>
-            
-        </div>
-        <form action="./quiz_playing.php" method="post">
-        <input type="hidden" name="id_quiz" value="<?= $typeQuiz["id"] ?>"/>
-        <button type="submit">Let's go !</button>
-        </form>
-    </article>
-    <?php
-}
-?>
-</section>
+
+
+
+            <h2 class="mt-[35%] text-center font-rubik text-primary text-2xl font-medium">Bienvenue <?= $_SESSION["pseudo"]  ?></h2>
+            <h1 class="font-changa text-[2.5rem] text-center mb-5 text-primary">Choix du quiz</h1>
+        </header>
+
+        <main class="flex flex-col gap-10">
+ <?php
+            foreach ($typesQuiz as $typeQuiz) {
+
+            ?>
+           
+                <!-- CHOIX*************************************************************************** 
+   ******************************************************************************************** 
+   ******************************************************************************************** 
+   ********************************************************************************************-->
+                <article
+                    class="relative h-full max-w-full bg-primaryopacity border-[5px] border-primary rounded-[1rem] shadow-inner-box">
+
+
+
+
+                    <!-- EN TETE -->
+
+                    <h3 class="text-light font-changa my-3 text-stroke"><?= $typeQuiz["name"] ?></h3>
+                    <!-- Trait décoratif -->
+                    <div class="border-t-8 border-light rounded-full mx-4 mb-6"></div>
+
+                    <div class="flex flex-col gap-4">
+
+                        <!-- RANK 1  -->
+                    <!-- RANK 1  -->
+                    <div
+                        class="flex flex-row items-center justify-between mx-10 p-6 bg-white border-b border-primary rounded-3xl shadow-2xl">
+                        <div class="flex gap-2">
+
+                            <span
+                                class="inline-flex items-center justify-center w-12 h-12 border-4 border-gray-custom rounded-full text-center bg-transparent">
+                                1
+                            </span>
+
+                            <div class="flex flex-col ml-4">
+                                <span class="text-black text-xl font-bold ">Davis Curtis</span>
+                                <span class="text-graycustom text-lg font-semibold">2,569 points</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="../images/Phone - Quiz sélection/Gold.png" alt="Médaille d'or"
+                                class="w-[2.5rem] inline-flex items-center justify-center ">
+                        </div>
+                    </div>
+
+                    <!-- RANK 2  -->
+                    <div
+                        class="flex flex-row items-center justify-between mx-10 p-6 bg-white border-b border-primary rounded-3xl shadow-2xl">
+                        <div class="flex gap-2">
+
+                            <span
+                                class="inline-flex items-center justify-center w-12 h-12 border-4 border-gray-custom rounded-full text-center bg-transparent">
+                                2
+                            </span>
+
+                            <div class="flex flex-col ml-4">
+                                <span class="text-black text-xl font-bold ">Alena Donin</span>
+                                <span class="text-graycustom text-lg font-semibold">1,469 points</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="../images/Phone - Quiz sélection/Silver.png" alt="Médaille d'argent"
+                                class="w-[2.5rem] inline-flex items-center justify-center ">
+                        </div>
+                    </div>
+
+
+                    <!-- RANK 3  -->
+                    <div
+                        class="flex flex-row items-center justify-between mx-10 p-6 bg-white border-b border-primary rounded-3xl shadow-2xl">
+                        <div class="flex gap-2">
+
+                            <span
+                                class="inline-flex items-center justify-center w-12 h-12 border-4 border-gray-custom rounded-full text-center bg-transparent">
+                                3
+                            </span>
+
+                            <div class="flex flex-col ml-4">
+                                <span class="text-black text-xl font-bold ">Graig Gouse</span>
+                                <span class="text-graycustom text-lg font-semibold">1,053 points</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="../images/Phone - Quiz sélection/Bronze.png" alt="Médaille de Bronze"
+                                class="w-[2.5rem] inline-flex items-center justify-center ">
+                        </div>
+                    </div>
+                </div>
+
+
+                    <form action="./quiz_playing.php" method="post">
+                        <input type="hidden" name="id_quiz" value="<?= $typeQuiz["id"] ?>" />
+                        <button type="submit" class="btn-custom2 btn-custom2:hover btn-custom:focus">
+                            Let's go !</button>
+                    </form>
+
+                </article>
+
+
+            <?php
+            }
+            ?>
+
 
 
 </body>
+
 </html>
--- source/quiz_playing.php
+++ source/quiz_playing.php
@@ -17,7 +17,6 @@ try {
         ':id_quiz' => $_POST['id_quiz']
     ]);
     $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 } catch (PDOException $error) {
     echo "Erreur lors de la requete : " . $error->getMessage();
 }
@@ -42,31 +41,107 @@ try {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Questions</title>
+    <link rel="stylesheet" href="../assets/output.css">
+    <link href="../assets/style.css" rel="stylesheet">
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <section class="questions">
+<body class="fond-quadrille animate-bg-scroll overflow-x-hidden bg-[#6E433C] bg-opacity-50">
+    <!-- Ajout d'une div avec une couleur de fond et une opacité de 40% -->
+    <!-- <div class="absolute inset-0 bg-[#6E433C] bg-opacity-50 z-0 h-screen w-full"></div> -->
 
-        <?php
-        foreach ($questions as $question) {
-        ?>
-            <div class="questions">
-                <?= $question['content'] ?>
+    <!-- Section principale -->
+    <section
+        class="relative h-full max-w-full m-10 p-3 bg-gradient-clair-orange border-t-[7px] border-l-[7px] border-r-[15px] border-b-[15px] border-primary rounded-[42px]">
+        <!-- Image en haut de la section -->
+        <div class="absolute top-[-1rem] left-0 w-full flex justify-center items-start">
+            <img src="../images/Phone - Quiz question/Group 32.png" alt="Image décorative" class="w-[120%] max-w-none">
         </div>
 
+
+        <header class="mt-[45%] mb-10 flex items-center gap-[15%]">
+            <div class="relative">
+                <img src="../images/Phone - Quiz question/Décoration haut gauche/Question.png" alt="Numéro question"
+                    class="scale-120">
+                <div class="absolute inset-0 flex justify-center items-center mt-7">
+                    <span class="text-light font-changa text-6xl">1</span>
+                </div>
+
+                <!-- Texte '/6' en haut à droite de l'image -->
+                <div class="absolute top-0 right-0 p-2 text-light font-changa text-lg mt-3">
+                    /6
+                </div>
+            </div>
+
+            <!-- "Jeux Vidéo" centré sur la droite -->
+            <h1 class="font-changa text-[2.25em] text-primary">Jeux Vidéo</h1>
+        </header>
+
+        <article class="m-4">
             <?php
-        }
+            foreach ($questions as $question) {
             ?>
-    </section>
+                <div class="bg-red-500 text-light font-chara rounded-lg p-6 mb-5">
+                    <?= $question['content'] ?>
+                </div>
+
+                <!-- LES REPONSES -->
+                <div class="flex flex-col mx-3 gap-5 mb-5">
+                    <!-- 1 -->
+                    <div class="bg-blue-300 text-light font-chara rounded-lg p-6">
+                        Réponse 1
+                    </div>
+                    <!-- 2 -->
+                    <div class="bg-blue-300 text-light font-chara rounded-lg p-6">
+                        Réponse 2
+                    </div>
+                    <!-- 3 -->
+                    <div class="bg-blue-300 text-light font-chara rounded-lg p-6">
+                        Réponse 3
+                    </div>
+                    <!-- 4 -->
+                    <div class="bg-blue-300 text-light font-chara rounded-lg p-6">
+                        Réponse 4
+                    </div>
+                </div>
+                <button type="submit" class="btn-custom2 btn-custom2:hover btn-custom2:focus">
+                    Valider
+                </button>
+
+        </article>
 
 
-    
+        <footer>
+            <div class="mb-4">
+                <label for="timer" class="text-lg font-bold">Temps :</label>
+                <progress id="timer" value="0" max="100" class="w-full h-5 rounded-full bg-red-600">
+                    <div class="bg-[#6E433C] h-full rounded-lg"></div>
+                </progress>
+            </div>
+        
+            <div class="flex justify-center items-center gap-4 text-red-600">
+                <span class="font-bold text-3xl">"PLAYER"</span>
+                <br>
+                <span id="score" class="text-lg">Votre score : 0 pts</span>
+            </div>
+        </footer>
+
+
+
+    <?php
+            }
+    ?>
+
+
+</section>
+
 </body>
 
 </html>
