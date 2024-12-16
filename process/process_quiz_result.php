13d9150d489bef69a1c757cce27c19f3503289e5
esteranodin <esteranodin@hotmail.fr>
2024-12-16 12:33:19 +0100

    ADD: REPONSE

--- process/process_quiz_result.php
+++ process/process_quiz_result.php
@@ -1,13 +1,32 @@
 <?php 
 
-
-print_r($_POST["answers"]);
-foreach ($$_POST["answer"] as $key ) {
-print_r ($key);
-}
-
-$sql = ""
+require_once '../utils/connect_db.php';
+session_start();
 
 
+// Question permet de choper l'id de la question, id_answer c'est un tableau qui stocke un tableau à l'index 0 à chaque fois et l'id de la réponse que l'utilisateur a choisi
+foreach ($_POST["answers"] as $id_question => $id_answer) {
+
+        // Je recupère dans la base de donnée l'id de la réponse de l'utilisateur pour chaque questions.
+        $sql = "SELECT id FROM answer WHERE id_question = :id_question AND id = :id_answer"; 
+
+        try {
+            
+            $insertStmt = $pdo->prepare($sql);
+           
+            $insertStmt->bindParam(':id_question', $id_question, PDO::PARAM_INT); 
+            $insertStmt->bindParam(':id_answer', $id_answer, PDO::PARAM_INT); 
+            
+            $insertStmt->execute();
+   
+
+        } catch (\Throwable $th) {
+            // Gérer l'erreur si nécessaire
+            echo "Une erreur s'est produite : " . $th->getMessage();
+        }
+    }
+    
+    $result = $insertStmt->fetch(PDO::FETCH_ASSOC);
+      var_dump ($result);
 
 ?>
